c27b3513e42f29fb1b4219649cd2693bca8b369f
Andy <ag@ssofb.co.uk>
2024-04-30 22:25:08 +0100

    basic

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Weight Dashboard</title>
+
+    </head>
+    <body>
+    
+        <form>
+
+            <label for="date">Date:</label>
+            <input type="date" id="date" name="date" value="<?php ?>" />
+
+            <label for="weight">Weight:</label>
+            <input type="weight" id="weight" name="weight" min="40" max="200" />
+
+        </form>
+        
+
+    </body>
+</html>
